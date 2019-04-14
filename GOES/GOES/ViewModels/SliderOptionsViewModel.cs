2006474ba614b52afee9c91fb8d2b5a0ea3e1b37
Jimmy Garrido <jimmygarrido@outlook.com>
2019-04-14 00:17:46 -0700

    html parse

--- GOES/GOES/ViewModels/SliderOptionsViewModel.cs
+++ GOES/GOES/ViewModels/SliderOptionsViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
 using Xamarin.Forms;
 
@@ -41,13 +43,25 @@ namespace GOES.ViewModels
 
             client = new HttpClient();
             LoadData();
+            OnPropertyChanged("Satellites");
         }
 
         async void LoadData()
         {
-            var page = await client.GetStringAsync("http://rammb-slider.cira.colostate.edu/?sat=goes-17&z=3&im=12&ts=1&st=0&et=0&speed=130&motion=loop&map=0&lat=0&opacity%5B0%5D=1&hidden%5B0%5D=0&pause=0&slider=-1&hide_controls=1&mouse_draw=0&follow_feature=0&follow_hide=0&s=rammb-slider&sec=conus&p%5B0%5D=geocolor&x=7448.6796875&y=3745");
+            var html = await client.GetStringAsync("http://rammb-slider.cira.colostate.edu/?sat=goes-17&z=3&im=12&ts=1&st=0&et=0&speed=130&motion=loop&map=0&lat=0&opacity%5B0%5D=1&hidden%5B0%5D=0&pause=0&slider=-1&hide_controls=1&mouse_draw=0&follow_feature=0&follow_hide=0&s=rammb-slider&sec=conus&p%5B0%5D=geocolor&x=7448.6796875&y=3745");
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
 
-            Debug.WriteLine(page);
+            var selectNodes = doc.DocumentNode.SelectSingleNode("//body").ChildNodes
+                .First(n => n.Id == "pluginWrapper").ChildNodes
+                .First(n => n.Id == "controlWrapper").ChildNodes
+                .First(n => n.Id == "productSelectorHolder").Elements("select");
+
+            foreach(var satNode in selectNodes.First(n => n.Id == "satelliteSelectorChange").Elements("option"))
+            {
+                Satellites.Add(satNode.InnerText);
+            }
+
         }
     }
 }
